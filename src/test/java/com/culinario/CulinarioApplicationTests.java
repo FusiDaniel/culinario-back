8c0b275b6dbfaa8dfc7b2c07c18b431c748c0511
Tiago Simionato <tiago.simionato5@gmail.com>
2025-04-03 22:04:56 -0300

    fix: remove unused test

--- src/test/java/com/culinario/CulinarioApplicationTests.java
+++ src/test/java/com/culinario/CulinarioApplicationTests.java
@@ -1,13 +1,8 @@
 package com.culinario;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CulinarioApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
